97e56670dbb48daacfa5ffaecde680909062d4e2
Adam Midvidy <amidvidy@gmail.com>
2014-09-17 16:45:54 -0400

    reenable Seq test

--- src/mongo/dbtests/jsobjtests.cpp
+++ src/mongo/dbtests/jsobjtests.cpp
@@ -1316,6 +1316,20 @@ namespace JsobjTests {
             }
         };
 
+        class Seq {
+        public:
+            void run() {
+                for ( int i=0; i<10000; i++ ) {
+                    OID a;
+                    OID b;
+                    
+                    a.init();
+                    b.init();
+
+                    ASSERT( a < b );
+                }
+            }
+        };
     } // namespace OIDTests
 
 
@@ -2271,6 +2285,7 @@ namespace JsobjTests {
             add< OIDTests::increasing >();
             add< OIDTests::ToDate >();
             add< OIDTests::FromDate >();
+            add< OIDTests::Seq >();
             add< ValueStreamTests::LabelBasic >();
             add< ValueStreamTests::LabelShares >();
             add< ValueStreamTests::LabelDouble >();
