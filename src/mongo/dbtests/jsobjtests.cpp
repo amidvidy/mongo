70a17ae979b8b44e312406e948ac55e5b5674dcd
Adam Midvidy <amidvidy@gmail.com>
2014-09-19 10:54:30 -0400

    fix jsobjtests

--- src/mongo/dbtests/jsobjtests.cpp
+++ src/mongo/dbtests/jsobjtests.cpp
@@ -1242,7 +1242,7 @@ namespace JsobjTests {
                 OID b;
 
                 a.init();
-                b.init( a.str() );
+                b.init( a.toString() );
 
                 ASSERT( a == b );
             }
@@ -1258,9 +1258,9 @@ namespace JsobjTests {
                 BSONObj o = b.obj();
                 keyTest(o);
 
-                ASSERT( o["a"].__oid().str() == "000000000000000000000000" );
-                ASSERT( o["b"].__oid().str() == "000000000000000000000000" );
-                ASSERT( o["c"].__oid().str() != "000000000000000000000000" );
+                ASSERT( o["a"].__oid().toString() == "000000000000000000000000" );
+                ASSERT( o["b"].__oid().toString() == "000000000000000000000000" );
+                ASSERT( o["c"].__oid().toString() != "000000000000000000000000" );
 
             }
         };
