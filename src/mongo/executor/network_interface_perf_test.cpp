1ced191e95fce2e4fe980df191e1eaf3e3a0eccc
Adam Midvidy <amidvidy@gmail.com>
2015-11-04 12:59:36 -0500

    add latency stats to perf test

--- src/mongo/executor/network_interface_perf_test.cpp
+++ src/mongo/executor/network_interface_perf_test.cpp
@@ -30,7 +30,10 @@
 
 #include "mongo/platform/basic.h"
 
+#include <chrono>
 #include <exception>
+#include <algorithm>
+#include <limits>
 
 #include "mongo/base/status_with.h"
 #include "mongo/bson/bsonmisc.h"
@@ -55,37 +58,79 @@ namespace mongo {
 namespace executor {
 namespace {
 
-const std::size_t numOperations = 16384;
+const std::size_t numOperations = 1000000;
 
+struct LatencyStats {
+    LatencyStats(std::vector<stdx::chrono::microseconds> opTimes) {
+        std::vector<std::tuple<stdx::chrono::microseconds, std::size_t>> opTimesWithIndexes;
 
-int timeNetworkTestMillis(std::size_t operations, NetworkInterface* net) {
-    net->startup();
-    auto guard = MakeGuard([&] { net->shutdown(); });
+        for (std::size_t i = 0; i < opTimes.size(); ++i) {
+            opTimesWithIndexes.emplace_back(opTimes[i], i);
+        }
 
+        std::sort(std::begin(opTimesWithIndexes), std::end(opTimesWithIndexes));
+
+        min = opTimesWithIndexes[0];
+        max = opTimesWithIndexes[opTimesWithIndexes.size() - 1];
+        percentile50 = opTimesWithIndexes[std::floor(opTimesWithIndexes.size() * 0.5)];
+        percentile99 = opTimesWithIndexes[std::floor(opTimesWithIndexes.size() * 0.99)];
+        percentile999 = opTimesWithIndexes[std::floor(opTimesWithIndexes.size() * 0.999)];
+        percentile9999 = opTimesWithIndexes[std::floor(opTimesWithIndexes.size() * 0.9999)];
+    }
+
+    using time_with_index = std::tuple<stdx::chrono::microseconds, std::size_t>;
+
+    time_with_index min;
+    time_with_index max;
+
+    time_with_index percentile50;
+    time_with_index percentile99;
+    time_with_index percentile999;
+    time_with_index percentile9999;
+};
+
+using clock = stdx::chrono::high_resolution_clock;
+
+std::vector<stdx::chrono::microseconds> executeOps(std::size_t operations, NetworkInterface* net) {
     auto fixture = unittest::getFixtureConnectionString();
     auto server = fixture.getServers()[0];
 
     std::atomic<int> remainingOps(operations);  // NOLINT
     stdx::mutex mtx;
     stdx::condition_variable cv;
-    Timer t;
 
     // This lambda function is declared here since it is mutually recursive with another callback
     // function
     stdx::function<void()> func;
 
+    // only accessed by this thread
+    std::vector<clock::time_point> startTimes;
+    startTimes.reserve(operations);
+
+    // only accessed by asio thread (until cv wait ends)
+    std::vector<clock::time_point> endTimes;
+    endTimes.reserve(operations);
+
+    // only accessed by this thread
+    std::vector<stdx::chrono::microseconds> opTimes;
+    opTimes.reserve(operations);
+
     const auto bsonObjPing = BSON("ping" << 1);
 
     const auto callback = [&](StatusWith<RemoteCommandResponse> resp) {
         uassertStatusOK(resp);
+        endTimes.emplace_back(clock::now());
+
         if (--remainingOps) {
             return func();
         }
+
         stdx::unique_lock<stdx::mutex> lk(mtx);
         cv.notify_one();
     };
 
     func = [&]() {
+        startTimes.emplace_back(clock::now());
         net->startCommand(makeCallbackHandle(),
                           {server, "admin", bsonObjPing, bsonObjPing, Milliseconds(-1)},
                           callback);
@@ -96,25 +141,42 @@ int timeNetworkTestMillis(std::size_t operations, NetworkInterface* net) {
     stdx::unique_lock<stdx::mutex> lk(mtx);
     cv.wait(lk, [&] { return remainingOps.load() == 0; });
 
-    return t.millis();
+
+    for (std::size_t i = 0; i < startTimes.size(); ++i) {
+        opTimes.emplace_back(
+            stdx::chrono::duration_cast<stdx::chrono::microseconds>(endTimes[i] - startTimes[i]));
+    }
+    // Throw away the first op since it includes connect latency.
+    opTimes.erase(std::begin(opTimes));
+    return opTimes;
 }
 
 TEST(NetworkInterfaceASIO, SerialPerf) {
     NetworkInterfaceASIO::Options options{};
     options.streamFactory = stdx::make_unique<AsyncStreamFactory>();
     options.timerFactory = stdx::make_unique<AsyncTimerFactoryASIO>();
-    NetworkInterfaceASIO netAsio{std::move(options)};
+    NetworkInterfaceASIO net{std::move(options)};
 
-    int duration = timeNetworkTestMillis(numOperations, &netAsio);
-    int result = numOperations * 1000 / duration;
-    log() << "THROUGHPUT asio ping ops/s: " << result << std::endl;
-}
+    net.startup();
+    auto guard = MakeGuard([&] { net.shutdown(); });
 
-TEST(NetworkInterfaceImpl, SerialPerf) {
-    NetworkInterfaceImpl netImpl{};
-    int duration = timeNetworkTestMillis(numOperations, &netImpl);
-    int result = numOperations * 1000 / duration;
-    log() << "THROUGHPUT impl ping ops/s: " << result << std::endl;
+    auto opTimes = executeOps(numOperations, &net);
+
+    LatencyStats stats{std::move(opTimes)};
+
+    auto to_string = [](LatencyStats::time_with_index twi) -> std::string {
+        std::stringstream ss;
+        ss << "duration - " << std::get<0>(twi) << ", opIndex - " << std::get<1>(twi);
+        return ss.str();
+    };
+
+    log() << "latency stats:";
+    log() << "\tmin - " << to_string(stats.min);
+    log() << "\t50th percentile - " << to_string(stats.percentile50);
+    log() << "\t99th percentile - " << to_string(stats.percentile99);
+    log() << "\t99.9th percentile - " << to_string(stats.percentile999);
+    log() << "\t99.99th percentile - " << to_string(stats.percentile9999);
+    log() << "\tmax - " << to_string(stats.max);
 }
 
 }  // namespace
