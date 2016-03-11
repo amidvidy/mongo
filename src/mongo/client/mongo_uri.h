1f114e961e926a5a690e987714475477a301c12c
Adam Midvidy <amidvidy@gmail.com>
2016-03-11 12:41:04 -0500

    changes for qb

--- src/mongo/client/mongo_uri.h
+++ src/mongo/client/mongo_uri.h
@@ -110,10 +110,10 @@ public:
         return _connectString.type();
     }
 
-private:
     explicit MongoURI(const ConnectionString connectString)
         : _connectString(std::move(connectString)){};
 
+private:
     MongoURI(ConnectionString connectString,
              const std::string& user,
              const std::string& password,
